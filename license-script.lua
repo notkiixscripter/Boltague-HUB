f0125c1975e1220316b1349c99fe322f58bd39ca
notkiixscripter <notkiixborge@gmail.com>
2026-07-23 16:34:40 -0500

    Update license-script.lua

--- license-script.lua
+++ license-script.lua
@@ -1,7 +1,6 @@
 -- license-script.lua (en GitHub)
 return {
     [3301543079]  = true, -- @kamilmADRW21
-    [10155796405] = true, -- @KOU_kaig34
     [10424906759] = true, -- @Thunfr4198
     [9657784020] = true, -- @Astenrobby49745
     [9466953543] = true, -- @Erginsusil811
