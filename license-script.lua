27b14ccc31c5c4dcb23f39bdda455c1b9d319d71
notkiixscripter <notkiixborge@gmail.com>
2026-07-26 15:55:14 -0500

    Update license-script.lua

--- license-script.lua
+++ license-script.lua
@@ -8,6 +8,8 @@ return {
     [8015322065] = true, -- @utrerx7aq
     [3798308583] = true, -- @deniskim63548
     [10176043314] = true, -- @CHOCOZZ54
-    [2899716783] = true -- @THENIHNO
+    [2899716783] = true, -- @THENIHNO
+    [10859117155] = true, -- @Hengbiggs44714
+    [10163380313] = true -- @Piratahacks5
     -- Agrega o quita IDs aquí cuando quieras
 }
