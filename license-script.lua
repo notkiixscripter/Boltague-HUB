756f2e011db61f82e6db378d21d97a4390f85826
notkiixscripter <notkiixborge@gmail.com>
2026-09-08 09:59:48 -0500

    Update license-script.lua

--- license-script.lua
+++ license-script.lua
@@ -23,6 +23,7 @@ return {
     [10912394322] = true, -- @el gay de utrex
     [10884538736] = true,
     [9580699294] = true,
-    [4485321898] = true
+    [4485321898] = true,
+    [5014679091] = true
     -- Agrega o quita IDs aquí cuando quieras
 }
