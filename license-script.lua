cb5b2cdd69c98a86d049cfa93aea251100f816f4
notkiixscripter <notkiixborge@gmail.com>
2026-09-18 21:50:00 -0500

    Update license-script.lua

--- license-script.lua
+++ license-script.lua
@@ -29,6 +29,7 @@ return {
     [10767295019] = true,
     [5510998492] = true,
     [10634472893] = true,
-    [9576330865] = true 
+    [9576330865] = true,
+    [7732760787] = true
     -- Agrega o quita IDs aquí cuando quieras
 }
